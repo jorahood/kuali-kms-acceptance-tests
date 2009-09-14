9d1073897386eea3c7dada1f969dcad331d4962b
Andy Orahood <jorahood@indiana.edu>
2009-09-14 15:33:34 -0400

    commit steps files but not feature files

--- features/step_definitions/Sage_will_do_stuff_steps.rb
+++ features/step_definitions/Sage_will_do_stuff_steps.rb
@@ -1,8 +1,12 @@
 Given /^I go to Sage's homepage$/ do
-  pending
+  @browser.open('http://140.182.144.96:8080/sagejpaspring/Home.do')
 end
 
-Then /^I should not get a "([^\"]*)" error$/ do |arg1|
-  pending
+Then /^I should see a web page$/ do
+@browser.is_element_present("css=body")
+end
+
+Then /^I should see "([^\"]*)"$/ do |arg1|
+  @browser.is_text_present("Hello").should be_true
 end
 
