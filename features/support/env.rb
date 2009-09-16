ba02fb79eebdbe83d11d4cc993fdc674ff7c39cf
Andy Orahood <jorahood@indiana.edu>
2009-09-16 16:01:48 -0400

    Moved the non-rails includes to World calls

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@ require 'spec/expectations'
 # Comment out the next line if you don't want Cucumber Unicode support
 require 'cucumber/formatter/unicode'
 
-require 'webrat/selenium'
+require 'webrat'
 require 'webrat/core/matchers'
 require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
 
@@ -16,6 +16,8 @@ Webrat.configure do |config|
   config.application_framework = :external
 
 end
+World(Webrat::Methods)
+World(Webrat::Selenium::Methods)
 
 #this is necessary to have webrat “wait_for” the response body to be available
 #when writing steps that match against the response body returned by selenium
