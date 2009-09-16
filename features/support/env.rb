13c7a2c8db382bf1805b36342820b7fba4238988
Andy Orahood <jorahood@indiana.edu>
2009-09-16 10:29:05 -0400

    webrat a-ok

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,12 +4,10 @@ require 'spec/expectations'
 # Comment out the next line if you don't want Cucumber Unicode support
 require 'cucumber/formatter/unicode'
 
-require 'webrat'
 require 'webrat/selenium'
 require 'webrat/core/matchers'
 require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
 
-World(Webrat::Selenium::Matchers) #mix into Cucumber's World obj
 
 Webrat.configure do |config|
 
@@ -20,9 +18,8 @@ Webrat.configure do |config|
 end
 
 #this is necessary to have webrat “wait_for” the response body to be available
-
 #when writing steps that match against the response body returned by selenium
-
+World(Webrat::Selenium::Matchers) #mix into Cucumber's World obj
 
 require File.expand_path(File.dirname(__FILE__) + '/jira_links_formatter')
 
@@ -31,26 +28,3 @@ require File.expand_path(File.dirname(__FILE__) + '/jira_links_formatter')
 #Bumps.configure { use_server 'http://localhost:1981'
 ##  format_results_with(JiraLinksFormatter)
 #}
-
-#for selenium support w/o webrat until I can figure out what's going on with not finding RAILS_ROOT
-#https://webrat.lighthouseapp.com/projects/10503/tickets/214-cucumber-selenium_webrat-example-completely-failing
-#copied the below out of cucumber/examples/selenium/features/support/env.rb
-
-#require 'selenium'
-#
-## "before all"
-#browser = Selenium::SeleniumDriver.new("localhost", 4444, "*chrome", "http://localhost", 15000)
-#
-#Before do
-#  @browser = browser
-#  @browser.start
-#end
-#
-#After do
-#  @browser.stop
-#end
-#
-## "after all"
-#at_exit do
-#  browser.close rescue nil
-#end
