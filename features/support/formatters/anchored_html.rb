59f4e1a2ab04a0d04c539a17256252dd306f6ec9
jorahood <jorahood@indiana.edu>
2011-07-25 11:45:44 -0500

    never mind this is unnecessary; the images are openable in the iframe just fine

--- features/support/formatters/anchored_html.rb
+++ features/support/formatters/anchored_html.rb
@@ -10,16 +10,6 @@ module Cucumber
         dashed_name = lines[0].gsub(/\s+/, "-")
         @builder.a(:name => dashed_name)
       end
-
-      # Customize image embedder to make images expanded by default for use in confluence pages
-      def embed_image(src, label)
-        id = "img_#{@img_id}"
-        @img_id += 1
-        @builder.span(:class => 'embed') do |pre|
-          pre << %{<a href="" onclick="img=document.getElementById('#{id}'); img.style.display = (img.style.display == 'block' ? 'none' : 'block');return false">#{label}</a><br>&nbsp;
-          <img id="#{id}" style="display: none" src="#{src}"/>}
-        end
-      end
     end
   end
 end
