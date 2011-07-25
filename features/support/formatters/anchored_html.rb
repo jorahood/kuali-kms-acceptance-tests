f6c6c3676de04154f1437e341cd3171b6879ecdc
jorahood <jorahood@indiana.edu>
2011-07-25 12:09:35 -0500

    move the named anchors below the feature name and explanation since we already have that just below us on the feature Confluence page

--- features/support/formatters/anchored_html.rb
+++ features/support/formatters/anchored_html.rb
@@ -4,9 +4,9 @@ module Cucumber
   module Formatter
     class AnchoredHtml < Html
 
-      def before_feature(feature)
-        super(feature)
-        lines = feature.name.split(/\r?\n/)
+      def before_feature_element(feature_el)
+        super(feature_el)
+        lines = feature_el.feature.name.split(/\r?\n/)
         dashed_name = lines[0].gsub(/\s+/, "-")
         @builder.a(:name => dashed_name)
       end
